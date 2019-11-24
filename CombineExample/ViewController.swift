9a39be4b4dfd0997b16c9736aa92f018c1c75005
Fernando Gomes <lfgsnando@gmail.com>
2019-11-24 15:48:45 -0300

    Initial Commit

---
+++ CombineExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CombineExample
+//
+//  Created by Fernando Gomes on 24/11/19.
+//  Copyright © 2019 Fernando Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
